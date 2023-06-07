c2f0ac95fafa5ee36b9a8d2eb02d508eee625c19
Johan Schuijt <johan.schuijt@ftm.nl>
2023-06-07 16:49:38 +0200

    simplify extraction, remove recursion

--- src/pagestream/__init__.py
+++ src/pagestream/__init__.py
@@ -5,11 +5,8 @@ Handle PDF pagestreams with PikePDF and split them by outline
 from re import search
 from sys import setrecursionlimit
 from logging import info
-from typing import List
 from pathlib import Path
 from pikepdf._qpdf import Pdf, Page
-from pikepdf.models.outlines import OutlineItem
-from itertools import tee, chain
 
 __version__ = "0.1.4"
 
@@ -17,85 +14,49 @@ __version__ = "0.1.4"
 # cause overflows in the CPython implementation
 setrecursionlimit(1024 * 1024)
 
-def flatten(items: List[OutlineItem]):
-    """Recursively flatten a outline tree"""
-    if len(items) == 0:
-        return items
-    if len(items[0].children) > 0:
-        return flatten(items[0].children) + flatten(items[1:])
-    return items[:1] + flatten(items[1:])
-
-def is_valid_outlineitem(item: OutlineItem):
-    """Check wether outline item has a page destination"""
-    if item.action is None:
-        return False
-    else:
-        # https://opensource.adobe.com/dc-acrobat-sdk-docs/library/pdfmark/pdfmark_Actions.html
-        # TODO - There's more valid actions than only `FitH`
-        return item.action.get('/D').as_list()[1] == '/FitH'
-
 class PDFPageStream:
     """Represents a PDF file that consists of a stream of merged PDF documents"""
     def __init__(self, path: Path):
         self.pdf = Pdf.open(path)
 
-    def get_outline_items(self):
-        """Get valid outlineitems (items with destination)"""
+    def get_embedded_documents(self):
         with self.pdf.open_outline() as outline:
-            return list(filter(is_valid_outlineitem, flatten(outline.root)))
+            for item in outline.root:
+                item = item.to_dictionary_object(self.pdf)
 
-    def can_extract_by_outline(self):
-        """Could this PDF document stream be seperated by PDF outline"""
-        # Some outlines are to short
-        items = self.get_outline_items()
-        if len(items) <= 1:
-            return False
+                # Could this be an actual embedded document?
+                title = item.get('/Title')
+                if title is None:
+                    continue
+                # TODO - Intent here is to not extract single pages that are
+                # just named something like "page <x>", there's better ways to do this
+                regex = "|".join(['Pagina\s?\d+', 'Page\s?\d+', '_Pagina_'])
+                is_literal_page = search(regex, str(title)) is not None
+                is_single_page = item.get('/Count') is None
+                if is_literal_page and is_single_page:
+                    continue
 
-        # Some outlines just have references to each page in the stream
-        # TODO - This regex tests for dutch and english outline items that all
-        # spell "Page", this could be improved by checking if every outline
-        # item only references subsequent pages
-        regex = "|".join(['Pagina\s?\d+', 'Page\s?\d+', '_Pagina_'])
-        if all(map(lambda i: search(regex, i.title) is not None, items)):
-            return False
+                # Create new PDF & append from src
+                pdf = Pdf.new()
+                with pdf.open_metadata() as meta:
+                    meta['dc:title'] = str(title)
 
-        # Some outlines have references to the same page in the document
-        pages = map(lambda i: Page(i.action.get('/D')[0]).index, items)
-        a, b = tee(pages)
-        if len(set(a)) != len(list(b)):
-            return False
+                if not is_single_page:
+                    next = item.get('/First')
+                    while next is not None:
+                        pdf.pages.append(Page(next.get('/Dest')[0]))
+                        next = next.get('/Next')
+                else:
+                    pdf.pages.append(Page(item.get('/A').get('/D')[0]))
 
-        return True
-
-    def extract(self):
-        """Split PDF file into seperate files"""
-        items = self.get_outline_items()
-        page_indexes = map(lambda i: Page(i.action.get('/D')[0]).index, items)
-        titles = map(lambda i: i.title, items)
-
-        # Get 2 iterators over the page numbers & advance the second iterator by one
-        from_pages, to_pages = tee(page_indexes)
-        next(to_pages)
-        # Add last page of PDF as end of last document & merge back into 1 iterator
-        documents = zip(titles, from_pages, chain(to_pages, [len(self.pdf.pages)]))
-
-        for title, from_page, to_page in documents:
-            # Create new PDF & append from src
-            pdf = Pdf.new()
-            with pdf.open_metadata() as meta:
-                meta['dc:title'] = title
-
-            for page in self.pdf.pages[from_page:to_page]:
-                pdf.pages.append(page)
-
-            yield pdf
+                yield pdf
 
     def extract_to(self, output_path):
         """Output split files into path"""
         if not output_path.exists():
             output_path.mkdir(parents=True)
 
-        for pdf in self.extract():
+        for pdf in self.get_embedded_documents():
             meta = pdf.open_metadata()
             title = meta['dc:title']
 
--- src/pagestream/cli.py
+++ src/pagestream/cli.py
@@ -3,6 +3,7 @@
 import sys
 import click
 import logging
+from __init__ import PDFPageStream
 
 @click.group(help="PDF pagestream utility")
 def cli() -> None:
@@ -10,8 +11,13 @@ def cli() -> None:
     logging.basicConfig(stream=sys.stderr, level=logging.INFO, format=fmt)
 
 @cli.command("contents", help="Show identified documents in pagestream")
-@click.argument("archive")
-def contents(archive):
-    click.echo(f"Extract {archive}")
+@click.argument('filename', type=click.Path(exists=True))
+def contents(filename):
+    pagestream = PDFPageStream(filename)
+    for pdf in pagestream.get_embedded_documents():
+        with pdf.open_metadata() as meta:
+            print(meta['dc:title'])
+        print("- " + str(len(pdf.pages)) + " pages")
 
-
+if __name__ == "__main__":
+    cli()
